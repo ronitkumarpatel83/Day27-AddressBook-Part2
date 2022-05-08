fcc03b3803984d41ddba9c1ceeb81f4714e6324b
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-05-08 19:59:52 +0530

    [Ronit]Added : Retrive Contact and introduce date in sql in vs

---
+++ SQLQuery1.sql
@@ -0,0 +1,14 @@
+﻿create procedure spUpdateContactInformation
+
+@First_Name varchar(200),
+@Last_Name varchar(200),
+@Phone bigint
+As
+Update addressbook set First_Name = @First_Name where Last_Name = @Last_Name and Phone = @phone;
+
+use address_book_service_DB;
+
+ select * from addressbook;
+
+ ALTER TABLE addressbook
+ADD Date varchar(255);
